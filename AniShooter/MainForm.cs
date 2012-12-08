1cb449eaae5b19fc2449a5cd866fe0ca2f95af5d
intrueder <intrueder@xakep.ru>
2012-12-08 17:02:06 +0200

    Global hotkey used

--- AniShooter/MainForm.cs
+++ AniShooter/MainForm.cs
@@ -8,21 +8,27 @@ using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
 
 namespace AniShooter
 {
     public partial class MainForm : Form
     {
         IDisposable subscription;
+        private readonly KeyboardHookListener _keyboardHookManager;
 
         public MainForm()
         {
             InitializeComponent();
             double sec = 0.5;
 
-            var mouse = Observable.FromEventPattern<MouseEventArgs>(this, "MouseClick").Do(ep => Trace.WriteLine("clicked"));
+            _keyboardHookManager = new KeyboardHookListener(new GlobalHooker());
+            _keyboardHookManager.Enabled = true;
+
+            var hotKey = Observable.FromEventPattern<KeyPressEventArgs>(_keyboardHookManager, "KeyPress").Where(ep => ep.EventArgs.KeyChar == 'a').Do(ep => { ep.EventArgs.Handled = true; Trace.WriteLine("hotkey handled"); });
             var generator = Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(sec)).Select(s => CaptureScreen());
-            subscription = generator.TakeUntil(mouse.Throttle(TimeSpan.FromSeconds(sec * 2))).TakeLast(5)
+            subscription = generator.TakeUntil(hotKey.Throttle(TimeSpan.FromSeconds(sec * 2))).TakeLast(5)
                                     .Subscribe(bmp => saveBmp(bmp));
         }
 
@@ -30,7 +36,7 @@ namespace AniShooter
         {
             try
             {
-                bmp.Save("E:\\shot" + DateTime.Now.Ticks + ".png", ImageFormat.Png);
+                bmp.Save("D:\\shot" + DateTime.Now.Ticks + ".png", ImageFormat.Png);
                 Trace.WriteLine("saved");
             }
             catch (Exception e)
